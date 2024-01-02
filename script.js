0c9bee68525ee56b3eb46515fb3ccaffa9a95b2b
mudassarmajeed5 <152526939+mudassarmajeed5@users.noreply.github.com>
2024-01-02 22:04:57 +0500

    Update script.js
    Fixed alert popup

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 function Date_of_Birth() {
+    let year = document.getElementById("year").value;
+    let month = document.getElementById("month").value;
+    let days = document.getElementById("days").value;
+
     document.getElementById("calculateage").addEventListener("click", () => {
         if (days == '') {
             alert("Enter a date");
         }
         else if (month == '') {
-            alert("Enter Month")
+            alert("Enter Month");
         }
-        else if (year >= 2024 || year>='') {
+        else if (year >= 2024 || year === '') {
             alert("Please enter a recent date");
         }
         else {
-            let year = document.getElementById("year").value;
-            let month = document.getElementById("month").value;
-            let days = document.getElementById("days").value;
             let birthdate = new Date(year, month - 1, days);
             console.log(birthdate);
             let currentDate = new Date();
             let ageInMilliseconds = currentDate - birthdate;
-            let c_years = ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000)
-            let years = Math.floor(c_years)
+            let c_years = ageInMilliseconds / (365.25 * 24 * 60 * 60 * 1000);
+            let years = Math.floor(c_years);
             let c_month = (c_years - years) * 12;
             let months = Math.floor(c_month);
             let c_days = Math.floor((c_years * 365.25 - years * 365.25 - months * (365.25 / 12)));
@@ -26,7 +27,7 @@ function Date_of_Birth() {
             document.getElementById("set_months").innerHTML = months;
             document.getElementById("set_days").innerHTML = c_days;
         }
-    })
-
+    });
 }
+
 Date_of_Birth();
